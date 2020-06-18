741c6a28c5ce45b508558ed77f9f87d54ede9e2d
Kevin Amado <kamadorueda@gmail.com>
2020-06-18 02:28:26 -0500

    refac(back): split code

--- src/tracers/function/__init__.py
+++ src/tracers/function/__init__.py
@@ -18,15 +18,8 @@ from typing import (
     Tuple,
 )
 
-# Constants
-CHAR_SPACE = chr(0x20)
-CHAR_INFO = chr(0x1F6C8) + CHAR_SPACE
-CHAR_CHECK_MARK = chr(0X2713)
-CHAR_BROKEN_BAR = chr(0xA6)
-CHAR_SUPERSCRIPT_ONE = chr(0x00B9)
-
-LOOP_CHECK_INTERVAL: float = 0.01
-LOOP_SKEW_TOLERANCE: float = 0.25
+# Local libraries
+from tracers.constants import *
 
 # Containers
 Frame = NamedTuple('Frame', [
