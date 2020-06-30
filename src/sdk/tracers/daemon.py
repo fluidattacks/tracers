a850e4ed4c4f81bae1c2773b7969d078109e4dc2
Kevin Amado <kamadorueda@gmail.com>
2020-06-30 00:01:59 -0500

    feat(back): notify new system

--- src/sdk/tracers/daemon.py
+++ src/sdk/tracers/daemon.py
@@ -3,7 +3,9 @@ import asyncio
 from collections import deque
 from threading import Thread
 from typing import (
+    Any,
     Deque,
+    Dict,
     Tuple,
 )
 
@@ -35,12 +37,20 @@ _RESULTS_QUEUE: Deque[DaemonResult] = deque()
 
 
 async def daemon() -> None:
+    if GRAPHQL_CLIENT:
+        success, msg = await send_system_to_server(client=GRAPHQL_CLIENT)
+
+        if success:
+            LOGGER_DAEMON.info('New system created: %s', CONFIG.system_id)
+        else:
+            LOGGER_DAEMON.error('Error creating system: %s', msg)
+
     while await asyncio.sleep(1.0, result=True):
         results = tuple(iter_except(_RESULTS_QUEUE.pop, IndexError))
         results_len = len(results)
 
         if GRAPHQL_CLIENT and results:
-            success, msg = await send_results_to_server(
+            success, msg = await send_transactions_to_server(
                 client=GRAPHQL_CLIENT,
                 results=results,
             )
@@ -58,49 +68,83 @@ def send_result_to_daemon(
     _RESULTS_QUEUE.appendleft(result)
 
 
-async def send_results_to_server(
+async def request_server(
+    *,
+    client: aiogqlc.GraphQLClient,
+    query: str,
+    variables: Dict[str, Any] = {},
+) -> Tuple[bool, str]:
+    msg: str
+    success: bool
+
+    try:
+        await client.execute(query=query, variables=variables)
+    except aiohttp.ClientError as exception:
+        msg, success = str(exception), False
+    else:
+        msg, success = '', True
+
+    return success, msg
+
+
+async def send_system_to_server(
+    *,
+    client: aiogqlc.GraphQLClient,
+) -> Tuple[bool, str]:
+    return await request_server(
+        client=client,
+        query="""
+            mutation(
+                $systemId: String!
+            ) {
+                putSystem(
+                    systemId: $systemId
+                ) {
+                    success
+                }
+            }
+        """,
+        variables={
+            'systemId': CONFIG.system_id,
+        },
+    )
+
+
+async def send_transactions_to_server(
     *,
     client: aiogqlc.GraphQLClient,
     results: Tuple[DaemonResult, ...],
 ) -> Tuple[bool, str]:
-    try:
-        await client.execute(
-            query="""
-                mutation(
-                    $systemId: String!
-                    $transactions: [TransactionInput!]!
+    return await request_server(
+        client=client,
+        query="""
+            mutation(
+                $systemId: String!
+                $transactions: [TransactionInput!]!
+            ) {
+                putTransactions(
+                    systemId: $systemId
+                    transactions: $transactions
                 ) {
-                    putTransactions(
-                        systemId: $systemId
-                        transactions: $transactions
-                    ) {
-                        success
-                    }
+                    success
                 }
-            """,
-            variables={
-                'systemId': CONFIG.system_id,
-                'transactions': [
-                    {
-                        'initiator': result.stack[0].function,
-                        'stack': json_dumps(result.stack),
-                        'totalTime': str(delta(
-                            result.stack[0].timestamp,
-                            result.stack[-1].timestamp,
-                        )),
-                    }
-                    for result in results
-                ],
-            },
-        )
-    except aiohttp.ClientError as exception:
-        msg: str = str(exception)
-        success: bool = False
-    else:
-        msg = ''
-        success = True
-
-    return success, msg
+            }
+        """,
+        variables={
+            'systemId': CONFIG.system_id,
+            'transactions': [
+                {
+                    'initiator': result.stack[0].function,
+                    'stack': json_dumps(result.stack),
+                    'totalTime': str(delta(
+                        result.stack[0].timestamp,
+                        result.stack[-1].timestamp,
+                    )),
+                }
+                for result in results
+            ],
+        },
+    )
 
 
 # Side effect: Start an asynchronous daemon server
