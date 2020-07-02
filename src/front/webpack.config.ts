89a82f91c65a61e06c84cc3c2c8316925cb867d9
Kevin Amado <kamadorueda@gmail.com>
2020-07-01 22:52:50 -0500

    feat(front): add package.json

---
+++ src/front/webpack.config.ts
@@ -0,0 +1,77 @@
+// tslint:disable-next-line:match-default-export-name
+import CopyWebpackPlugin from "copy-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import path from "path";
+import webpack from "webpack";
+
+const config: webpack.Configuration = {
+  bail: true,
+  entry: {
+    index: "./src/index.tsx",
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              configFile: "tsconfig.webpack.json",
+              transpileOnly: true,
+            },
+          },
+        ],
+      },
+      {
+        include: /node_modules/,
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+        ],
+      },
+      {
+        exclude: /node_modules/,
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    filename: "[name].js",
+    futureEmitAssets: true,
+    path: path.resolve(__dirname, "docs"),
+  },
+  plugins: [
+    new CopyWebpackPlugin([{
+      from: "static",
+    }]),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  stats: {
+    children: false,
+    colors: true,
+    modules: false,
+  },
+};
+
+export = config;
